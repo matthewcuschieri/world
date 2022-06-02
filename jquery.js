cea85169872362da0d30da6784d3c044a6f198c1
mattcoosh <mcuschie@risd.edu>
2022-06-01 20:38:53 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,12 +1,24 @@
 console.log("check");
 
 $(document).ready(function () {
+  $("#basicdesignlink").click(function () {
+    $("#basicwrap").toggleClass("basicdesignlink");
+  });
+  $("#basicdesignlink").click(function () {
+    $(".basicinfo").toggleClass("basicdesignlink");
+  });
   $("#designlink").click(function () {
     $(".kunstinfo").toggleClass("designlink");
   });
 
   $("#designlink").click(function () {
     $("#kunstwrap").toggleClass("designlink");
+  });
+  $("#readdesignlink").click(function () {
+    $("#readwrap").toggleClass("readdesignlink");
+  });
+  $("#readdesignlink").click(function () {
+    $(".readinfo").toggleClass("readdesignlink");
   });
   $("#gdfwdesignlink").click(function () {
     $(".gdfwinfo").toggleClass("gdfwdesignlink");
