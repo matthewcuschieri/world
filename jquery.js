78a2493272e6030c07cb3121316b198121a93d2e
mattcoosh <mcuschie@risd.edu>
2023-01-13 15:22:00 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -36,3 +36,89 @@ $(document).ready(function () {
     $("#hiwrap").toggleClass("hidesignlink");
   });
 });
+
+// $(".col3").on("click", function (e) {
+//   $("#opt3").toggleClass("active");
+
+//   if (!$("#").hasClass("active")) {
+//     $("#listbar").toggleClass("active");
+//     $("#wrapper").toggleClass("active");
+//   }
+//   //you can list several class name
+// });
+// function {
+
+// changeOne = document.getElementsById("opt1");
+// changeTwo = document.getElementsByClassName("col3");
+// headIndex = "+";
+
+// setInterval(function () {
+//   $("#opt1").replaceWith('<div id="opt2">' + "</div>"); if (!S)
+// }, 9000);
+
+$(".about").on("click", function (e) {
+  $("#opt3").toggleClass("active");
+  $("#minus").toggleClass("active");
+
+  if (!$("#opt3").hasClass("active")) {
+    $("#opt2").removeClass("active");
+    $("#opt1").removeClass("active");
+    $("#minus").toggleClass("active");
+    if (!$("#opt3:not(.active)").toggleClass("active")) {
+    }
+  }
+
+  e.preventDefault();
+});
+$("#minus").on("click", function (e) {
+  $("#opt2").addClass("active");
+  $("#aboutPage").removeClass("active");
+  $("#opt3").removeClass("active");
+  $("#opt1").removeClass("active");
+  $("#plus").addClass("active");
+  $("#minus").toggleClass("active");
+
+  e.preventDefault();
+});
+$("#plus").on("click", function (e) {
+  $("#opt1").toggleClass("active");
+
+  $("#aboutPage").toggleClass("active");
+
+  $("#plus").removeClass("active");
+  if (!$("#opt1").hasClass("active")) {
+    $("#opt2").removeClass("active");
+    $("#opt3").removeClass("active");
+  }
+  if (!$("#opt1").hasClass("active")) {
+    $("#opt2").removeClass("active");
+    $("#opt3").removeClass("active");
+  }
+  e.preventDefault();
+});
+let timeoutId;
+$(document).ready(function () {
+  console.log("ready!");
+  $("#opt1").toggleClass("active");
+  $("#opt2").addClass("active");
+  $("#aboutPage").toggleClass("active");
+  timeoutId = setTimeout(function () {
+    $("#aboutPage").removeClass("active");
+    $("#plus").toggleClass("active");
+    $("#opt1").removeClass("active");
+  }, 2000);
+
+  // if (!$("#opt3").hasClass("active")) {
+  //   clearTimeout(timeoutId);
+  // }
+  $("#opt1".animate({ opacity: "0.4" }, "slow"));
+});
+
+!$("#opt3").hasClass("active");
+{
+  clearTimeout(timeoutId);
+}
+!$("#opt3").on("click", function (e) {
+  clearTimeout(timeoutId);
+  e.preventDefault();
+});
