f134d10232e23825af11d562701ef29fe4b7ddb0
mattcoosh <mcuschie@risd.edu>
2023-01-13 15:22:05 -0500

    Update my.js

--- my.js
+++ my.js
@@ -45,3 +45,16 @@ function myFunction() {
   var element = document.getElementById("matthewcuschieri");
   element.classList.toggle("aboutmain");
 }
+
+// var changeOne = document.getElementsById("opt1");
+// var changeTwo = document.getElementsByClassName("col3");
+// var headIndex = "+";
+// var fullHead =
+//   "Previously he worked for Johnson & Johnson Brand Design Studio, Brian Belott and Ben La Rocco as a studio intern. He was recently invited to attend Anderson Ranch Arts Center, was featured in New American Paintings no. 152, the College Hill Independent, and exhibited in the Gelman Gallery (Memories and How We Hold Them) and Online Playroom (Love is in the Air).";
+
+// function change() {
+//   changeOne[0].innerHTML = "Matthew Cuschieri" + headIndex;
+//   changeTwo[0].innerHTML = "+";
+// }
+// // run change function every 3 seconds
+// setInterval(change, 1000);
