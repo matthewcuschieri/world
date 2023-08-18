1469266b20f9f72bc48ffdf17b82220018d18c52
mattcoosh <mcuschie@risd.edu>
2023-08-18 18:15:31 -0400

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -118,3 +118,14 @@ $("#plus").on("click", function (e) {
 //     }
 //   }, 2000);
 // });
+
+const blurredImageDiv = document.querySelector(".blur-load");
+const img = blurredImageDiv.querySelector("img");
+function loaded() {
+  blurredImageDiv.classList.add("loaded");
+}
+if (img.complete) {
+  loaded();
+} else {
+  img.addEventListener("load", loaded);
+}
